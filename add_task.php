a45d2ee82f810463d90df8d2479219b0040e19c6
lasya-danda <lasyadanda4406@gmail.com>
2025-07-24 15:41:59 +0530

    add_task.php

--- add_task.php
+++ add_task.php
@@ -67,7 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <div class="sidebar">
     <h5>Task Management</h5>
-    <a href="index.php">Dashboard</a>
+    <a href="dashboard.php">Dashboard</a>
     <a href="add_task.php">Add Task</a>
     <a href="report.php">Reports</a>
     <a href="calendar.php">Calendar</a>
